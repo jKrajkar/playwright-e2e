26f3fcfa76f95fd5d2cc3043c44c58c378bf7f44
Josef Krajkář <josef.krajkar@blogic.cz>
2025-06-29 15:32:00 +0200

    Add cI cron job

--- tests/test-myDock.spec.ts
+++ tests/test-myDock.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('Test myDock login + Tasker', async ({ page }) => {
-    await page.goto('https://vyvoj.mydock.sab-apps.blogic.cz/');
+    const baseUrl = process.env.TEST_URL || 'https://vyvoj.mydock.sab-apps.blogic.cz/';
+    await page.goto(baseUrl);
 
-    expect(page).toHaveURL('https://vyvoj.mydock.sab-apps.blogic.cz/');
+    expect(page).toHaveURL(baseUrl);
 
     const loginLocator = page.getByRole('textbox', { name: 'přihlašovací jméno' });
     await expect(loginLocator).toBeVisible();
